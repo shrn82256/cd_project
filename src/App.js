3db40400e1cec7834e2c9c8f8eb884d8583864ef
Your Name <you@example.com>
2019-03-25 19:40:10 +0530

    search bar added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {BrowserRouter,Switch,Route} from 'react-router-dom';
 import Dashboard from './components/Dashboard'
 import SchoolDetails from './components/SchoolDetails';
+// require("bootstrap/less/bootstrap.less");
+
 
 class App extends Component {
   render() {
--- src/components/Schools.js
+++ src/components/Schools.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
 import { NavLink } from 'react-router-dom';
+// import Pagination from "react-js-pagination";
+// import InfiniteScroll from 'react-infinite-scroller';
+
 
 class Schools extends Component {
     state = {
         data: [],
+        // activePage: 1,
     }
+
+    // handlePageChange(PageNumber) {
+    //     console.log(`active page is ${PageNumber}`);
+    //     this.setState({
+    //         activePage: PageNumber,
+    //     })
+    //     fetch("http://schoolhub-heroku.herokuapp.com/").then(res => res.json()).then(data =>
+    //         this.setState({
+    //             data: data.slice(PageNumber * 10 - 10, PageNumber * 10),
+    //         })
+    //     )
+    // }
     componentDidMount() {
         fetch("http://schoolhub-heroku.herokuapp.com/").then(res => res.json()).then(data =>
             this.setState({
@@ -12,7 +28,34 @@ class Schools extends Component {
             })
         )
     }
+    // loadFunc=()=>{
+    //     console.log("loading")
+    //     if(!this.state.flag){
+    //         this.setState({
+    //             loaded:this.state.data.slice(0,10),
+    //         })
+    //     }
+    // }
+    
+    handleSearch(e){
+        let curruntList = [];
+        let newList =[];
+        if(e.target.value !== ""){
+            curruntList = this.state.data;
+            newList = curruntList.filter(items=>{
+                const cl = items.name.toLowerCase();
+                const filter = e.target.value.toLowerCase();
+                return cl.includes(filter);
+            })
+        }else{
+            newList = this.state.data;
+        }
+        this.setState({
+            data:newList
+        })
+    }
     render() {
+        console.log(this.props)
         const schoolList = this.state.data.map(res => {
             return (
                 <div className="data" key={res.pid}>
@@ -40,8 +83,34 @@ class Schools extends Component {
         })
         return (
             <div className="schools">
+            <form action="">
+                    <div className="input-field">
+                    <label htmlFor="search" className="label-icon"><i class="material-icons">search</i></label>
+                        <input type="search" id="search" onChange = {this.handleSearch.bind(this)}/>
+                        <i class="material-icons">close</i>
+                    </div>
+                </form>
+                {/* <InfiniteScroll
+                    pageStart={0}
+                    onClick={this.loadFunc}
+                    hasMore={true || false}
+                    loader={<div className="loader" key={0}>Loading ...</div>}
+                >
+                </InfiniteScroll> */}
                 {schoolList}
+
+                {/* <div className="center">
+                <Pagination
+                    activePage={this.state.activePage}
+                    itemsCountPerPage={10}
+                    totalItemsCount={450}
+                    pageRangeDisplayed={5}
+                    onChange={this.handlePageChange.bind(this)}
+                />
+                </div> */}
+
             </div>
+
         )
     }
 }
